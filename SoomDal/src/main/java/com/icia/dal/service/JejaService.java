ccbe7330b4f1242d805b0fc0fd0f5db915bb68d1
차승훈 <@DESKTOP-7988RO5>
2020-06-23 14:37:55 +0900

    jeja join by 승훈

---
+++ SoomDal/src/main/java/com/icia/dal/service/JejaService.java
@@ -0,0 +1,26 @@
+package com.icia.dal.service;
+
+import javax.inject.*;
+
+import org.springframework.stereotype.*;
+
+import com.icia.dal.dao.*;
+import com.icia.dal.entity.*;
+
+@Service
+public class JejaService {
+	@Inject
+	private JejaDao dao;
+
+	public void existsByEmail(String jEmail) {
+		String email = dao.existsByjEmail(jEmail);
+		// 나중에 예외처리 다시 해야함
+		if(email!=null)
+			throw new RuntimeException();
+	}
+
+	public void join(Jeja jeja) {
+		dao.insertJeja(jeja);
+	}
+	
+}
