e7825cf7b25496abd952f4b13c4de140e5f6beda
wjdgu <wjdgu@LAPTOP-KM0D2JJS>
2020-06-25 08:58:37 +0900

    requestboard read 작업 by 현석

--- SoomDal/src/main/java/com/icia/dal/controller/RequestBoardController.java
+++ SoomDal/src/main/java/com/icia/dal/controller/RequestBoardController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.*;
 
+import com.icia.dal.dto.*;
 import com.icia.dal.service.*;
 
 @Controller
@@ -15,8 +16,20 @@ public class RequestBoardController {
 	@Inject
 	private RequestBoardService service;
 	
-	//@GetMapping("/reqboard/read")
-	//public ModelAndView read(int rbNo) {
-		//return new ModelAndView("main").addObject("viewName","reqboard/read.jsp");
-	//}
+	@GetMapping("/reqboard/read")
+	public ModelAndView read(int rbNo, Principal principal) {
+		service.read(rbNo, principal.getName());
+		return new ModelAndView("main").addObject("viewName","reqboard/read.jsp");
+	}
+	
+	@GetMapping("/reqboard/write")
+	public ModelAndView write() {
+		return new ModelAndView("main").addObject("viewName","reqboard/write.jsp");
+	}
+	
+	@PostMapping("/reqboard/write")
+	public String write(RequestBoardDto.DtoForWrite dto, String rbWriter, Principal principal) {
+		dto.setRbWriter(principal.getName());
+		return "redirect:/reqboard/read?rbNo=" + service.write(dto);
+	}
 }
