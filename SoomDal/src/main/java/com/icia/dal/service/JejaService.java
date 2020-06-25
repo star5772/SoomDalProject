327edab0052bb9b389e87a0627e972e6969e7151
차승훈 <@DESKTOP-7988RO5>
2020-06-25 15:26:33 +0900

    jeja update and jeja login 수정 by 승훈

--- SoomDal/src/main/java/com/icia/dal/service/JejaService.java
+++ SoomDal/src/main/java/com/icia/dal/service/JejaService.java
@@ -2,10 +2,13 @@ package com.icia.dal.service;
 
 import javax.inject.*;
 
+import org.modelmapper.*;
 import org.springframework.security.crypto.password.*;
 import org.springframework.stereotype.*;
 
 import com.icia.dal.dao.*;
+import com.icia.dal.dto.*;
+import com.icia.dal.dto.JejaDto.*;
 import com.icia.dal.entity.*;
 
 @Service
@@ -13,7 +16,11 @@ public class JejaService {
 	@Inject
 	private JejaDao dao;
 	@Inject
+	private DAO authDao;
+	@Inject
 	private PasswordEncoder pwdEncoder;
+	@Inject
+	private ModelMapper modelMapper;
 
 	public void existsByEmail(String jEmail) {
 		String email = dao.existsByjEmail(jEmail);
@@ -29,11 +36,26 @@ public class JejaService {
 		// 나중에 회원가입 실패했을 시 예외처리 다시 해야함
 		if(dao.insertJeja(jeja)==0)
 			throw new RuntimeException();
+		authDao.insertAuthority(jeja.getJEmail(), "ROLE_JEJA");
 	}
 	
 	public void delete(String jEmail) {
 		// 제자 회원탈퇴
 		dao.deleteJeja(jEmail);
+	}
+	
+	public JejaDto.DtoForJejaRead read(String jEmail) {
+		Jeja jeja = dao.findById(jEmail);
+		JejaDto.DtoForJejaRead dto = modelMapper.map(jeja,JejaDto.DtoForJejaRead.class);
+		dto.setEmail(jeja.getJEmail()).setName(jeja.getJName()).setTel(jeja.getJTel());
+		System.out.println(dto);
+		return dto;
+	}
+
+	public void update(DtoForJejaUpdate dto, String jEmail) {
+		Jeja jeja = dao.findById(jEmail);
+		System.out.println("==========");
+		System.out.println(dto);
 	}
 
 }
